f98be8e762704c6b3a345cb4a480edc6035b9552
LUK-KUL <bizon674@gmail.com>
2023-02-05 13:41:29 +0100

    Update testing branch

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,6 +10,7 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("zmiany do nowego brancha");
+        System.out.println("dalsze testowanie celem nauki gita poprzez git hub desktop");
     }
 
 }
